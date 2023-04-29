77ba5333dc51ca4d9d994354a14a87ed6f89bbd7
harah24 <hhordofa1@gmail.com>
2023-04-29 13:19:13 -0400

    added some css and hamburger menu for mobile design

---
+++ script/script.js
@@ -0,0 +1,17 @@
+//define variables for the hamburger div and the nav unordered list
+
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+
+//addeventlistener to the hamburger menu to toggle back and forth 
+hamburger.addEventListener('click', () =>{
+  hamburger.classList.toggle('active');
+  navList.classList.toggle('active');
+})
+
+// click event to hide the menu once selection has been made 
+document.querySelectorAll('.nav-link').forEach(i => 
+i.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}))
