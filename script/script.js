a6353fc9a7a47eb61760f30f8da7454721880555
harah24 <hhordofa1@gmail.com>
2023-05-09 12:22:05 -0400

    added colour variables & updated sections with the colours

--- script/script.js
+++ script/script.js
@@ -1,8 +1,22 @@
-// nav toggle - select button and links  - define variables
-const navToggle = document.querySelector("#navToggle");
+
+//header section 
+// define variables that target the button and links
+
+const navToggle = document.querySelector("#nav-toggle");
 const nav = document.querySelector("#nav-links");
 
-//add event listener - arrow function to toggle b/w the css classes
+//add event listener to target the hamburger menu
+
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
 });
+
+
+// Contact(form)section 
+// form reset code from formspree.io
+
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
